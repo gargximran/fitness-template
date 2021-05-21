08d323146b6234231b0e1fb8bec0c64c92d97980
gargximran <gargximran@gmail.com>
2021-05-21 17:27:58 +0600

    disable eslint

--- src/Components/Hero/TopHeroSection/index.js
+++ src/Components/Hero/TopHeroSection/index.js
@@ -3,6 +3,7 @@ import './style.css'
 const TopHeroSection = ({hero, title}) => {
     return (
         <div className="w-full overlayMaker relative">
+            {/* eslint-disable-next-line */}
             <img src={hero} alt={'No image!'} className={'w-full'}/>
             <h2 className={'hero-title'}>{title}</h2>
         </div>
